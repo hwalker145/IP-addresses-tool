df4a13e4cc742b83752fad16eb41efb87c962978
hwalker145 <hwalker145@gmail.com>
2022-07-18 19:18:35 -0400

    Updated .gitignore, began test3

--- test/test3.cpp
+++ test/test3.cpp
@@ -1,1 +1,21 @@
 #include "../Includes/IPAddress.h"
+
+using namespace libxl;
+
+int main()
+{
+	try 
+	{
+		Book* B = xlCreateXMLBook();
+
+	}
+	catch (wchar_t * exception)
+	{
+		exit(1);
+	}
+	catch (...)
+	{
+		exit(1);
+	}
+	return 0;
+}
